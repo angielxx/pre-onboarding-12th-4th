43d8129a1b06c33d76480c703cd4f6fa42b3ad9f
angleixx <angielxx94@gmail.com>
2023-09-12 16:15:17 +0900

    design: globalStyle 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App.tsx';
+
+import theme from '@/styles/theme.ts';
+import GlobalStyle from '@/styles/GlobalStyle.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyle />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
